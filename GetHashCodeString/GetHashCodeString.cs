34fbb593c45461515f78c6717af27d7080a5c5f8
vanjs83 <tihomir.vanjurek@gmail.com>
2015-11-26 21:03:03 +0100

    solve

--- GetHashCodeString/GetHashCodeString.cs
+++ GetHashCodeString/GetHashCodeString.cs
@@ -6,7 +6,7 @@ namespace Vsite.CSharp
     {
         static void Main(string[] args)
         {
-            // TODO: Pokrenuti program i ispis preslikati u datoteku GetHashCodeString.txt pridruženu projektu
+            //  Pokrenuti program i ispis preslikati u datoteku GetHashCodeString.txt pridruženu projektu
             // U datoteci navesti koja dva znakovan niza daju najbliže brojeve
 
             Console.WriteLine("cat".GetHashCode());
@@ -14,6 +14,10 @@ namespace Vsite.CSharp
             Console.WriteLine("cap".GetHashCode());
             Console.WriteLine("car".GetHashCode());
             Console.WriteLine("cut".GetHashCode());
+            object o1 = new object();
+            object o2 = new object();
+            object o3 = o1;
+
 
             Console.ReadKey();
         }
